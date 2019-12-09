b886359bea0fdc13e3b2e24d895e70a9d5db9ef4
Assafm <assafe.cohen@gmail.com>
2019-12-09 16:31:26 +0200

    added tooltip for image

--- src/components/ChampionsGrid/Champion/Champion.js
+++ src/components/ChampionsGrid/Champion/Champion.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import classes from './Champion.module.css'
 import ChampionCard from './ChampionCard/ChampionCard'
+import Tooltip from '@material-ui/core/Tooltip';
+
+
 
 
 
 const Champion = (props) => {
-  const {name,icon} = props.champion;
+
+  const {name,icon,title} = props.champion;
 
   return (
     <div className={classes.Avatar}>
-        <img alt={name} src={icon} className={classes.AvatarImage}  />
+        <Tooltip title={title} >
+          <img alt={name} src={icon} className={classes.AvatarImage}  />
+        </Tooltip>
         <span className={classes.AvatarName}>{name}</span>
         <ChampionCard champion={props}/>
     </div>
